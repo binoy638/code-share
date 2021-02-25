d7a5d81d84b253eafdb8419ce1ae26081a4fbb5d
binoy638 <binoy638@gmail.com>
2021-02-25 21:43:10 +0530

    basic code editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Editor from "./components/Editor";
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+        `<html>
+  <body>${html}</body>
+  <style>${css}</style>
+  <script>${js}</script>
+  </html>
+  `
+      );
+    }, 250);
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="editor-section">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="output-section">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/Editor.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import "codemirror/addon/edit/closebrackets";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+
+function Editor({ displayName, language, value, onChange }) {
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+  const [open, setOpen] = useState(true);
+  return (
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        {displayName}
+        <button onClick={() => setOpen((prevOpen) => !prevOpen)}>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: "material",
+          lineNumbers: true,
+          autoCloseBrackets: true,
+        }}
+      />
+    </div>
+  );
+}
+
+export default Editor;
