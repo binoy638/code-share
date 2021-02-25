72feb342cb75829eff8be2d7f8b4beb6d75353e1
binoy638 <binoy638@gmail.com>
2021-02-25 22:22:36 +0530

    added local storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Editor from "./components/Editor";
 import "./App.css";
 import { useState, useEffect } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
 function App() {
-  const [html, setHtml] = useState("");
-  const [css, setCss] = useState("");
-  const [js, setJs] = useState("");
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
   const [srcDoc, setSrcDoc] = useState("");
 
   useEffect(() => {
